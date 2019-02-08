efeab98f0e75fb8d80dc5c82a6f01896e6b21fed
Jun-Young Lee <ljy.onefineday@snu.ac.kr>
2019-02-08 10:38:03 +0900

    start

--- app/src/main/java/com/example/get_location_app/MainActivity.java
+++ app/src/main/java/com/example/get_location_app/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.get_location_app;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private TextView txtLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.txtLocation = (TextView) findViewById(R.id.txtLocation);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
     }
+
+    mFusedLocationClient.getLastLocation()
+        .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+        @Override
+        public void onSuccess(Location location) {
+            // Got last known location. In some rare situations this can be null.
+            if (location != null) {
+                // Logic to handle location object
+            }
+        }
+    });
 }
+
+
